c31ab6e0b61ba63f8a38a1198823107253077209
SarinxaVanora <sarinxavanora@gmail.com>
2026-09-22 06:16:31 +0100

    version bump for assets

--- scripts/Build-Distribution.ps1
+++ scripts/Build-Distribution.ps1
@@ -2,7 +2,7 @@ param(
     [string]$GitHubRepository = "SarinxaVanora/RavaFit",
     [string]$Branch = "master",
     [string]$Configuration = "Release",
-    [string]$RuntimeVersion = "1.1.1",
+    [string]$RuntimeVersion = "1.1.2",
     [string]$BodiesVersion = "1.0.0",
     [string]$MinimumPluginVersion = "1.1.1",
     [switch]$RunTests
